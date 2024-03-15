e4817d165a11c21536bbfd68b0951a33975ac9f6
gracefeng05 <114462302+gracefeng05@users.noreply.github.com>
2024-03-15 14:48:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,31 +12,30 @@ DOWNLOAD_FOLDER = './assets'
 def hello_world():
     return "Hello, world!"
 
+@app.route('/download')
 def download():
     youtube_url = request.args.get('youtubeURL')
 
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '96',
-            }],
-            'update_extractor': True,
-            'nocheckcertificate': True,
-            'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(autonumber)s.%(ext)s'),
-            'verbose': True,
-        }
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '96',
+        }],
+        'update_extractor': True,
+        'nocheckcertificate': True,
+        'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(autonumber)s.%(ext)s'),
+        'verbose': True,
+    }
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([youtube_url])
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([youtube_url])
 
-        # Assuming only one MP3 file is downloaded
-        mp3_file_path = os.path.join(DOWNLOAD_FOLDER, os.listdir(DOWNLOAD_FOLDER)[0])
+    # Assuming only one MP3 file is downloaded
+    mp3_file_path = os.path.join(DOWNLOAD_FOLDER, os.listdir(DOWNLOAD_FOLDER)[0])
 
-        return send_file(mp3_file_path, as_attachment=True)
+    return send_file(mp3_file_path, as_attachment=True)
 
-    else:
-        return jsonify({"error": "Invalid request"}), 400
-        
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
